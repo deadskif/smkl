7fbfa533bce36ef64c547ab6f3a6e7d3b74f88ab
Vladimir Badaev <skif@nateks.ru>
2024-02-12 15:54:51 +0300

    Some improvments

--- rules.mk
+++ rules.mk
@@ -10,8 +10,8 @@ CXX = $(CROSS_COMPILE)g++
 endif
 
 INSTALL ?= install -D
-INSTALL_PROGRAM = $(INSTALL)
-INSTALL_DATA = $(INSTALL) -m 0644
+INSTALL_PROGRAM ?= $(INSTALL)
+INSTALL_DATA ?= $(INSTALL) -m 0644
 
 OBJS :=
 LIBS =
@@ -25,10 +25,9 @@ CFLAGS += $(addprefix -I,$(INCLUDE_DIRS) $(INCLUDE_C_DIRS))
 CXXFLAGS += $(addprefix -I,$(INCLUDE_DIRS) $(INCLUDE_CXX_DIRS))
 LDFLAGS += $(addprefix -L,$(LINK_DIRS))
 
-DEPSFLAGS = -MT $@ -MT $(@:.o=.d) -MF $(@:.o=.d) -MD -MP
-CFLAGS += $(DEPSFLAGS)
-CXXFLAGS += $(DEPSFLAGS)
+CPPFLAGS += -MT $@ -MT $(@:.o=.d) -MF $(@:.o=.d) -MD -MP
 
+SHAREDFLAGS ?= -fPIC
 # ###
 # Rules
 # ###
@@ -40,6 +39,10 @@ CXXFLAGS += $(DEPSFLAGS)
 # ###
 # Defines
 # ###
+
+# make-objs
+# $(1): name
+# $(2): objects list
 define make-objs
 $1_OBJS = $(addprefix $(MODULE),$(filter-out /%,$2)) $(filter /%,$2)
 
@@ -49,12 +52,19 @@ $$($1): $$($1_OBJS)
 $$($1): MODULE := $(if $(MODULE),$(MODULE),./)
 endef
 
+# make-link
+# $(1): name
+# $(2): libraries list
 define make-link
 $$($1):
-	$$(LINK.o) $(if $(MODULE),-L$(MODULE)) $3 $$(filter %.o,$$^) $$(LDLIBS) $(addprefix -l,$2) -o $$@
+	$$(LINK.o) $$(filter %.o,$$^) $$(LDLIBS) $(addprefix -l,$2) -o $$@
+$$($1): private LINK_DIRS += $(if $(MODULE),$(MODULE))
 endef
 
-
+# make-install
+# $(1): name
+# $(2): install variable name
+# $(3): dir variable to install
 define make-install
 ifneq ($1,$$($1))
 .PHONY: $1
@@ -68,38 +78,47 @@ install-$1: $$($1)
 install: install-$1
 endef
 
-# Make binary
+# make-binary
+# $(1): name
+# $(2): objects list
+# $(3): libraries list
 define make-binary
 $1 = $(MODULE)$1
 BINS += $$($1)
 
 all: $$($1)
-$(call make-objs,$1,$2,$3)
+$(call make-objs,$1,$2)
 $(call make-link,$1,$3)
 $(call make-install,$1,INSTALL_PROGRAM,bindir)
 endef
 
-# Make static library
+# make-static-library
+# $(1): name
+# $(2): objects list
 define make-static-library
 $1 = $(MODULE)lib$1.a
 LIBS += $$($1)
 
-$(call make-objs,$1,$2,$3)
+$(call make-objs,$1,$2)
 $(call make-install,$1,INSTALL_DATA,libdir)
 endef
 
-# Make shared library
+# make-shared-library
+# $(1): name
+# $(2): objects list
+# $(3): libraries list
 define make-shared-library
 $1 = $(MODULE)lib$1.so
 LIBS += $$($1)
 
 all: $$($1)
-$(call make-objs,$1,$2,$3)
-$(call make-link,$1,$3,-shared)
+$(call make-objs,$1,$2)
+$(call make-link,$1,$3)
 $(call make-install,$1,INSTALL_PROGRAM,libdir)
 
-$$($1_OBJS): CFLAGS += -fpic
-$$($1_OBJS): CXXFLAGS += -fpic
+$$($1_OBJS): CFLAGS += $(SHAREDFLAGS)
+$$($1_OBJS): CXXFLAGS += $(SHAREDFLAGS)
+$$($1): private LDFLAGS += -shared
 endef
 
 
